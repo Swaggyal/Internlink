a2e5ea0695e5f427563261cc6afb229d714cecee
Swaggyal <georgettegyamfi12@gmail.com>
2026-06-04 17:22:27 +0000

    Initial commit - Spring boot project setup

---
+++ src/test/java/com/Internlink/backend/InternlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Internlink.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
