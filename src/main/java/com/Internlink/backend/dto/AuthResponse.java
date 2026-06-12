8a1174f43bae14fa130f2a31c33fc967ed2dca79
Joyceline <joycelinemahama24@gmail.com>
2026-06-12 22:45:50 +0000

    More corrections in AuthResponse dto

--- src/main/java/com/Internlink/backend/dto/AuthResponse.java
+++ src/main/java/com/Internlink/backend/dto/AuthResponse.java
@@ -1,14 +1,18 @@
 package com.Internlink.backend.dto;
 
-import lombok.Data;
+import com.Internlink.backend.entity.UserRole;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    private boolean success;
+    private Long id;
+    private String email;
+    private String phoneNumber;
+    private UserRole role;
     private String message;
-    private String token;
+    private boolean success;
 }
